7ea18eae1de15f7da289073fe64f1038e8151154
sea-turt1e <h.yamada.bg@gmail.com>
2024-11-12 07:02:35 +0900

    TODO

--- app/initializer.py
+++ app/initializer.py
@@ -3,6 +3,7 @@ from app.usecases.normalize_text_usecase import NormalizeTextUsecase
 
 
 def initilize():
+    # TODO: 変更箇所
     get_normalize_controller = GetNormalizeController(NormalizeTextUsecase())
     return get_normalize_controller
 
