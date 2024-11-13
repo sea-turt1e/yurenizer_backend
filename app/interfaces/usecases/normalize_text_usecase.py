e2ce7866cd16573ad0f285c3680099838ce7039c
sea-turt1e <h.yamada.bg@gmail.com>
2024-11-13 12:10:32 +0900

    flg_options追加

--- app/interfaces/usecases/normalize_text_usecase.py
+++ app/interfaces/usecases/normalize_text_usecase.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
 
+from yurenizer import NormalizerConfig
+
 from app.schemes.normalize import NormalizeTextRequest, NormalizeTextResponse
 
 
 class NormalizeTextUseCaseInterface(ABC):
     @abstractmethod
-    async def execute(self, request: NormalizeTextRequest) -> NormalizeTextResponse:
+    async def execute(self, request: NormalizeTextRequest, config: NormalizerConfig) -> NormalizeTextResponse:
         pass
--- app/usecases/normalize_text_usecase.py
+++ app/usecases/normalize_text_usecase.py
@@ -2,13 +2,13 @@ from yurenizer import NormalizerConfig
 
 from app.interfaces.repositories.normalize_text_repositry import NormalizeRepositoryInterface
 from app.interfaces.usecases.normalize_text_usecase import NormalizeTextUseCaseInterface
-from app.schemes.normalize import NormalizeTextRequest, NormalizeTextResponse
+from app.schemes.normalize import NormalizeTextResponse
 
 
 class NormalizeTextUseCase(NormalizeTextUseCaseInterface):
     def __init__(self, repository: NormalizeRepositoryInterface):
         self.repository = repository
 
-    async def execute(self, text: str, config: NormalizerConfig = NormalizerConfig()) -> NormalizeTextResponse:
+    async def execute(self, text: str, config: NormalizerConfig) -> NormalizeTextResponse:
         normalized_text = await self.repository.normalize_text(text, config)
         return NormalizeTextResponse(text=normalized_text, length=len(normalized_text))
