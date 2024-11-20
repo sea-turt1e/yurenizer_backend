cffee1545a3fdf1f6b7e66dfe2a2f10def3ceb60
sea-turt1e <h.yamada.bg@gmail.com>
2024-11-20 12:02:13 +0900

    feat: CORS設定を環境変数から取得するように変更し、python-dotenvを追加

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import logging
+import os
 
 from api.api import api_router
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
@@ -9,9 +11,9 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-allows_origins = [
-    "http://localhost:5173",
-]
+# allow_originsを環境変数から取得
+load_dotenv()
+allows_origins = os.getenv("ALLOW_ORIGINS", "").split(",")
 logger.debug(f"allows_origins: {allows_origins}")
 
 # CORS設定を追加
@@ -22,6 +24,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(api_router)
 handler = Mangum(app)
